a8187b266bd9618aae764c835ea426486596d029
James Kingsbery <06jameskingsbery@gmail.com>
2011-02-06 14:25:08 -0500

    Added simple presentation layer for viewing User model.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 SampleApp::Application.routes.draw do
 
-  get "users/new"
+  resources :users
+
   match '/signup', :to => 'users#new'
   match '/contact', :to => 'pages#contact'
   match '/about', :to => 'pages#about'
