ca088b0f62b1ea9566433704101035f3ddf7e345
James Kingsbery <06jameskingsbery@gmail.com>
2011-02-11 21:39:15 -0500

    Refactored tests to remove common logic. Use sessions instead of cookies for storing session state.

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,7 +1,7 @@
 module SessionsHelper
   
   def sign_in(user)
-    cookies.permanent.signed[:remember_token] = [user.id, user.salt]
+    session[:remember_token] = [user.id, user.salt]
     self.current_user = user
   end
 
@@ -14,7 +14,7 @@ module SessionsHelper
   end
 
   def sign_out
-    cookies.delete(:remember_token)
+    session.delete(:remember_token)
     self.current_user = nil
   end
 
@@ -29,7 +29,7 @@ module SessionsHelper
     end
 
     def remember_token
-      cookies.signed[:remember_token] || [nil, nil]
+      session[:remember_token] || [nil, nil]
     end
   
 end
